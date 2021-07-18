28ed6c3b2caf3faea5f36f9ae778e45d6a150387
Lefteris Harteros <l.harteros@gmail.com>
2021-07-18 03:42:00 +0300

    top section, portfolio section, about section

--- components/sections/about.js
+++ components/sections/about.js
@@ -15,7 +15,7 @@ export default function About() {
                         <div className={styles.timeline__content}>
                             <h6>Athens University of Economics and Business</h6>
                             <p>Enrolled for BSc studies in Computer Science</p>
-                            <h2 className={styles.timeline__date}>Oct 2015</h2>
+                            <h6 className={styles.timeline__date}>Oct 2015</h6>
                         </div>
                     </div>
                     <div className={styles.timeline__block}>
@@ -25,7 +25,7 @@ export default function About() {
                             <p>Participated in Huawei&apos;s two week program in Beijing and Shenzhen where I
                                 experienced the
                                 Chinese Culture, studied Mandarin, and received ICT training</p>
-                            <h2 className={styles.timeline__date}>Nov 2018</h2>
+                            <h6 className={styles.timeline__date}>Nov 2018</h6>
                         </div>
                     </div>
                     <div className={styles.timeline__block}>
@@ -35,7 +35,7 @@ export default function About() {
                             <p>Graduated and received BSc in Computer Science with GPA 8.88/10 and specialization in
                                 Artificial Intelligence/Machine Learning, Databases and Knowledge Management and
                                 Information Systems and Information Security</p>
-                            <h2 className={styles.timeline__date}>Jul 2019</h2>
+                            <h6 className={styles.timeline__date}>Jul 2019</h6>
                         </div>
                     </div>
                     <div className={styles.timeline__block}>
@@ -43,7 +43,7 @@ export default function About() {
                         <div className={styles.timeline__content}>
                             <h6>Athens University of Economics and Business</h6>
                             <p>Enrolled for MSc studies in Computer Science</p>
-                            <h2 className={styles.timeline__date}>Oct 2019</h2>
+                            <h6 className={styles.timeline__date}>Oct 2019</h6>
                         </div>
                     </div>
                     <div className={styles.timeline__block}>
@@ -53,7 +53,7 @@ export default function About() {
                             <p>Graduated and received MSc in Computer Science with GPA 9.7/10
                                 and specialization in Artificial Intelligence/Machine Learning and Information
                                 Systems</p>
-                            <h2 className={styles.timeline__date}>Oct 2020</h2>
+                            <h6 className={styles.timeline__date}>Oct 2020</h6>
                         </div>
                     </div>
                     <div className={styles.timeline__block}>
@@ -63,7 +63,7 @@ export default function About() {
                             <p>Fulfilled military obligations at the Computer Science department where I worked as a
                                 Software Engineer developing the
                                 lethrodou.gr web app for Rhodes Officers Club</p>
-                            <h2 className={styles.timeline__date}>Aug 2021</h2>
+                            <h6 className={styles.timeline__date}>Aug 2021</h6>
                         </div>
                     </div>
                     <div className={styles.timeline__block}>
@@ -72,7 +72,7 @@ export default function About() {
                             <h6 css={css`font-weight: 400;
                               text-align: center;
                               margin-bottom: 0`}>Searcing for new opportunities and challenges</h6>
-                            <h2 className={styles.timeline__date}>Currently</h2>
+                            <h6 className={styles.timeline__date}>Currently</h6>
                         </div>
                     </div>
                 </section>
