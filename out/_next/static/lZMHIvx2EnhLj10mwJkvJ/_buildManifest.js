9d089cf702d7835bcae20a5d834d309c121e71d1
Lefteris Harteros <l.harteros@gmail.com>
2021-07-18 03:33:56 +0300

    Deploy gh-pages

---
+++ out/_next/static/lZMHIvx2EnhLj10mwJkvJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/427-c0a7b1717f7415ec0596.js","static/css/4efdb4fc09c6aca576bf.css","static/chunks/pages/index-7284caa32d17972bd16f.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
