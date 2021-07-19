9c0bb041bee511d1da97f6b4b629493d6ce01a4f
Lefteris Harteros <l.harteros@gmail.com>
2021-07-20 01:47:42 +0300

    contact fix text

--- components/sections/contact.js
+++ components/sections/contact.js
@@ -9,7 +9,7 @@ export default function Contact() {
             <Container>
                 <h2 css={css`text-align: center`}>Contact me</h2>
                 <p>If you have any questions about me or my projects feel free to contact me. You can also check out
-                    my <a href={"Resume_Charteros.pdf"} target={"_blank"} rel="noreferrer"><b>Resume</b></a> to find about more about me. I am open to conversetions and eager to talk about
+                    my <a href={"Resume_Charteros.pdf"} target={"_blank"} rel="noreferrer"><b>Resume</b></a> to find about more about me. I am open to conversations and eager to talk about
                     new opportunities. </p>
                 <Button to={"https://www.linkedin.com/in/lefteris-harteros/"}>Send a message</Button>
             </Container>
