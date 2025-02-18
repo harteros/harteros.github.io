c645688b95127e8df4df9da9ee1af0fadb03f083
Lefteris Charteros <lefteris.charteros@workable.com>
2025-02-18 16:44:12 +0200

    fix: remove nuxt studio dependency breaking build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,37 @@
 export default defineNuxtConfig({
   modules: [
-    '@nuxthq/studio',
     '@nuxt/eslint',
     '@nuxt/content',
     '@nuxt/ui',
     '@nuxt/image',
   ],
+
   components: [
     {
       path: '~/components',
       pathPrefix: false,
     },
   ],
+
   css: [
     '~/assets/css/main.css',
   ],
+
   colorMode: {
     preference: 'dark',
   },
+
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio',
+    },
+  },
+
   future: {
     compatibilityVersion: 4,
   },
   compatibilityDate: '2025-02-13',
+
   eslint: {
     config: {
       stylistic: true,
